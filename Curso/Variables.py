08eeff343e3e27816fadd28049b93453c3dfa540
Juan Pablo Mercado <juanpabloarizmendi203@gmail.com>
2026-04-01 18:53:21 -0600

    A

--- Curso/Variables.py
+++ Curso/Variables.py
@@ -6,4 +6,4 @@ Altura = 180.5
 Humano = False
 NombreCompleto = Nombre + " " + Apellido1 + " " + Apellido2
 
-print("Hello " + NombreCompleto + ", you are " + str(Edad) + " years old, and your height is " + str(Altura) + " cm " + " - Human: " + str(Humano))
+print("Hello " + NombreCompleto + ", how are you? You are " + str(Edad) + " years old, and your height is " + str(Altura) + " cm " + " - Human: " + str(Humano))
